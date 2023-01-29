89afa5a21b68cc1e98829d67ca192c40931389de
Asif Hasan <asif.hasan@gmail.com>
2023-01-29 18:34:34 +0600

    updated

--- lumos-visualization/rack-diagram/src/block/block_api.py
+++ lumos-visualization/rack-diagram/src/block/block_api.py
@@ -1,9 +1,6 @@
 #!/usr/bin/env python3
 
 import importlib
-from os import path
-
-from pysvg.parser import *
 
 from svg.svg_util import *
 from helper.logger import *
@@ -21,21 +18,14 @@ class BlockBase(object):
         # debug(f". {self.__class__.__name__} : {inspect.stack()[0][3]}")
         self._config = config
         self._data = data
+        self._type = self._data['type']
+        self._make = self._data['make']
+        self._model = self._data['model']
+        self._template = self._data['template']
+
+        self._svg_attrs = self._config['svg-attrs'][self._type][self._make][self._model][self._template]
+
         self._SVG = None
-
-
-    ''' get and open the template from data
-    '''
-    def open_template(self):
-        # which svg template to load?
-        svg_template_path = f"{self._config['dirs']['svg-template-dir']}/{self._data['type']}/{self._data['make']}/{self._data['model']}/{self._data['template']}.svg"
-        if not path.exists(svg_template_path):
-            error("no svg template [{svg_template_path}]")
-            return
-
-        # open as an SVG object
-        self._SVG = parse(svg_template_path)
-
 
 
 ''' Rack object
@@ -53,12 +43,22 @@ class Rack(BlockBase):
     '''
     def to_svg(self):
         # debug(f". {self.__class__.__name__} : {inspect.stack()[0][3]}")
-        self.open_template()
+        svg_template_path = f"{self._config['dirs']['svg-template-dir']}/{self._type}/{self._make}/{self._model}/{self._template}.svg"
+        self._SVG = open_svg_from_file(template_path=svg_template_path)
 
-        # TODO: do somnething arbitrary
+        # iterate children
+        for equipment in self._data['equipments']:
+            # get the u position
+            u_pos_list = equipment['position'].split('-')
+            u_start, u_end = int(u_pos_list[0]), int(u_pos_list[-1])
+            for u in range(u_start, u_end+1):
+                u_str = '{:0>2}'.format(u)
+                equipment_type = equipment['type']
+                equipment_make = equipment['make']
+                equipment_mpdel = equipment['model']
+                equipment_template = equipment['template']
+                message = f"position [{u_str}] occuied by equipment [{equipment_type}][{equipment_make}][{equipment_mpdel}][{equipment_template}]"
+                debug(message)
 
 
         return self._SVG
-
-
-
--- lumos-visualization/rack-diagram/src/svg/svg_util.py
+++ lumos-visualization/rack-diagram/src/svg/svg_util.py
@@ -7,6 +7,21 @@ import re
 import random
 import string
 
+from os import path
+
+from pysvg.parser import *
+
 from helper.logger import *
+
+
+''' get and open the template from data
+'''
+def open_svg_from_file(template_path):
+    if not path.exists(template_path):
+        error("no svg template [{template_path}]")
+        return
+
+    # open as an SVG object
+    return parse(template_path)
 
 
