d4cc9409e567ccb31d1b898bc937ebd0f5c8761d
Asif Hasan <asif.hasan@gmail.com>
2023-03-14 18:53:48 +0600

    updated

--- bpmn/bpmn-from-yml.bat
+++ bpmn/bpmn-from-yml.bat
@@ -13,7 +13,7 @@ set FMT=%~2
 
 :: yml-to-bpmn
 pushd .\src
-@REM .\yml-to-bpmn.py --config "../conf/config.yml" --yml "%YML%"
+.\yml-to-bpmn.py --config "../conf/config.yml" --yml "%YML%"
 
 if errorlevel 1 (
   popd
